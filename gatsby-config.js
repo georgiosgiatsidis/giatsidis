78b154c188923d5729f76edd8c1bb6cb0ecc6fb2
Georgios Giatsidis <georgiosgiatsidis@hotmail.gr>
2020-01-15 17:48:23 +0200

    Create blog template, load data from md files

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,6 +35,14 @@ module.exports = {
                 path: `${__dirname}/src/images`,
             },
         },
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                path: `${__dirname}/src/md`,
+                name: `md`,
+            },
+        },
+        `gatsby-transformer-remark`,
         `gatsby-transformer-sharp`,
         `gatsby-plugin-sharp`,
         {
