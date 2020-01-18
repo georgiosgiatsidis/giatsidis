1ee6af31afddfa2d8c041f96ffbf966d522703fb
Georgios Giatsidis <georgiosgiatsidis@hotmail.gr>
2020-01-19 00:25:42 +0200

    Update typography

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -5,11 +5,11 @@ import wordpress2016Theme from 'typography-theme-wordpress-2016';
 wordpress2016Theme.overrideThemeStyles = () => ({
     a: {
         color: 'var(--textLink)',
+        boxShadow: 'none',
     },
 });
 
-delete wordpress2016Theme.googleFonts;
-
+// delete wordpress2016Theme.googleFonts;
 const typography = new Typography(wordpress2016Theme);
 
 export const { scale, rhythm, options } = typography;
