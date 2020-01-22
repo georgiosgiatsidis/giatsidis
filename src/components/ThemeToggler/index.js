1ea7c83c57e704eb73018d021097281d2e375d61
Georgios Giatsidis <georgiosgiatsidis@hotmail.gr>
2020-01-22 22:17:26 +0200

    Move styles into separate files

--- src/components/ThemeToggler/index.js
+++ src/components/ThemeToggler/index.js
@@ -1,60 +1,6 @@
 import React from 'react';
-import styled, { css } from 'styled-components';
 import Toggler from './Toggler';
-
-const Wrapper = styled.div`
-    display: inline-flex;
-`;
-
-const Label = styled.label`
-    display: block;
-    position: relative;
-    width: 60px;
-    height: 30px;
-    background: var(--textLink);
-    border-radius: 40px;
-    user-select: none;
-    cursor: pointer;
-
-    &:after {
-        content: '';
-        position: absolute;
-        top: 0;
-        left: 0;
-        width: 30px;
-        height: 30px;
-        background: #fff;
-        border-radius: 40px;
-        transition: left 250ms ease;
-    }
-
-    ${props =>
-        props.checked &&
-        css`
-            &:after {
-                left: 100%;
-                transform: translateX(-100%);
-            }
-        `}
-`;
-
-const Input = styled.input`
-    width: 0;
-    height: 0;
-    visibility: hidden;
-`;
-
-const DarkIcon = styled.span`
-    display: block;
-    text-align: left;
-    padding-left: 9px;
-`;
-
-const LightIcon = styled.span`
-    display: block;
-    text-align: right;
-    padding-right: 5px;
-`;
+import { Wrapper, Label, Input, LightIcon, DarkIcon } from './styles';
 
 const ThemeToggler = () => {
     return (
