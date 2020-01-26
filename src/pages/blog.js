084db4a12f35fa6fe844124cf5b9bfae9f33a7d8
Georgios Giatsidis <georgegiats@trigonongroup.com>
2020-01-26 15:18:20 +0200

    Minimal blog page

---
+++ src/pages/blog.js
@@ -0,0 +1,75 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+import { Link, graphql } from 'gatsby';
+import { rhythm } from '../utils/typography';
+import Layout from '../components/Layout';
+import SEO from '../components/SEO';
+
+class BlogIndexTemplate extends React.Component {
+    render() {
+        const posts = this.props.data.allMarkdownRemark.edges;
+
+        return (
+            <Layout>
+                <SEO title="Blog" />
+                <aside></aside>
+                <main>
+                    {posts.map(({ node }) => {
+                        const title = node.frontmatter.title;
+
+                        return (
+                            <article key={title}>
+                                <header>
+                                    <h3
+                                        style={{
+                                            fontSize: rhythm(1),
+                                            marginBottom: rhythm(1 / 4),
+                                        }}
+                                    >
+                                        <Link
+                                            style={{ boxShadow: 'none' }}
+                                            to={node.frontmatter.path}
+                                            rel="bookmark"
+                                        >
+                                            {title}
+                                        </Link>
+                                    </h3>
+                                    <small>{node.frontmatter.date}</small>
+                                </header>
+                                <p
+                                    dangerouslySetInnerHTML={{
+                                        __html: node.frontmatter.spoiler,
+                                    }}
+                                />
+                            </article>
+                        );
+                    })}
+                </main>
+            </Layout>
+        );
+    }
+}
+
+export default BlogIndexTemplate;
+
+export const pageQuery = graphql`
+    query {
+        site {
+            siteMetadata {
+                title
+                description
+            }
+        }
+        allMarkdownRemark {
+            edges {
+                node {
+                    frontmatter {
+                        date(formatString: "MMMM DD, YYYY")
+                        path
+                        title
+                    }
+                }
+            }
+        }
+    }
+`;
