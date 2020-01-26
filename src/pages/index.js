da5c812b429595098a59b26ca0f32d8106e19d57
Georgios Giatsidis <georgegiats@trigonongroup.com>
2020-01-26 19:01:35 +0200

    rename seo module to SEO

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import Layout from '../components/Layout';
 import Profile from '../components/Profile';
-import SEO from '../components/seo';
+import SEO from '../components/SEO';
 
 const IndexPage = () => (
     <Layout fixed>
