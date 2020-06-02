05413a9824a1c9bcdc9d53b0d4ea8276a01652f3
Georgios Giatsidis <georgegiats@trigonongroup.com>
2020-06-02 23:38:48 +0300

    Under construction CV

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
 const Footer = () => (
-    <div className="p-4 p-4" style={{ background: 'var(--footer-bg)' }}>
+    <footer className="p-4 p-4" style={{ background: 'var(--footer-bg)' }}>
         <p>Made with ❤️</p>
-    </div>
+    </footer>
 );
 
 export default Footer;
---
+++ src/pages/cv/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Container from 'components/Container';
+import Layout from 'components/Layout';
+import SEO from 'components/SEO';
+import './styles.css';
+
+const IndexPage = () => (
+    <Layout>
+        <SEO title="CV" />
+        <Container>Under construction...</Container>
+    </Layout>
+);
+
+export default IndexPage;
